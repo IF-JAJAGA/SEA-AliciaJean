5efe5caa422eef3cba8dd96c4cc0614d6def2058
JeanMarchal <jean.marchal@hotmail.com>
2014-12-01 15:09:45 +0100

    Calculs des tailles et entrées de chaque tables de pages

--- vmem.c
+++ vmem.c
@@ -1,8 +1,8 @@
-#define PAGE_SIZE		4*1000 //4kB
-#define FIRST_LVL_TT_COUN	5
-#define FIRST_LVL_TT_SIZE	18*PAGE_SIZE // 18=(31-14+1) --> Par rapport au schéma 8.7
-#define SECOND_LVL_TT_COUN 	3
-#define SECOND_LVL_TT_SIZE	22*PAGE_SIZE // 22=(31-10+1) --> Par rapport au schéma 8.7
-#define TOTAL_TT_SIZE		
+#define PAGE_SIZE		4*1000/8 //4kB <=> 500 octets
+#define SECOND_LVL_TT_COUN 	512 // 2⁸ avec 8 = 19-12+1 (taille de Second Level tabe index dans l'adresse logique)
+#define SECOND_LVL_TT_SIZE	512*PAGE_SIZE // => 256 ko : Nombre d'entrées des tables de pages de niveau 2 multiplié par la taille d'une page
+#define FIRST_LVL_TT_COUN	4096 // 2¹² avec 12 = 31-20+1 (taille de First Level tabe index dans l'adresse logique)
+#define FIRST_LVL_TT_SIZE	4096*SECOND_LVL_TT_SIZE // => 1 048 576 ko : Nombre d'entrées des tables de pages de niveau 1 multiplié par la taille d'une table de pages de niveau 2
+#define TOTAL_TT_SIZE		//4096*FIRST_LVL_TT_SIZE <== Ca fait beaucoup... Environ 4 To... 
 
 
