092ce2125ba673bcd154d6c768a92f9f73349c20
Marchal Jean <jmarchal@if501-219-22.insa-lyon.fr>
2014-12-01 13:35:38 +0100

    Rajout de la ligne de taille total mais sans avoir trouvé le calcul final encore

--- vmem.c
+++ vmem.c
@@ -3,3 +3,6 @@
 #define FIRST_LVL_TT_SIZE	18*PAGE_SIZE // 18=(31-14+1) --> Par rapport au schéma 8.7
 #define SECOND_LVL_TT_COUN 	3
 #define SECOND_LVL_TT_SIZE	22*PAGE_SIZE // 22=(31-10+1) --> Par rapport au schéma 8.7
+#define TOTAL_TT_SIZE		
+
+
