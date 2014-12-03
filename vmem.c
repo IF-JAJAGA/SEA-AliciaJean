9a77b83cd7eeca5171b766a128d1d76f1ef9f496
Marchal Jean <jmarchal@if501-219-22.insa-lyon.fr>
2014-12-03 09:49:31 +0100

    finalisation des primitives define

--- vmem.c
+++ vmem.c
@@ -1,8 +1,10 @@
 #define PAGE_SIZE		4*1000/8 //4kB <=> 500 octets
-#define SECOND_LVL_TT_COUN 	512 // 2⁸ avec 8 = 19-12+1 (taille de Second Level tabe index dans l'adresse logique)
-#define SECOND_LVL_TT_SIZE	512*PAGE_SIZE // => 256 ko : Nombre d'entrées des tables de pages de niveau 2 multiplié par la taille d'une page
+#define SECOND_LVL_TT_COUN 	1024 // 2¹⁰ avec 8 = 19-12+1+2 (taille de Second Level tabe index dans l'adresse logique)
+#define SECOND_LVL_TT_SIZE	SECOND_LVL_TT_COUN*4 // 4096 o
 #define FIRST_LVL_TT_COUN	4096 // 2¹² avec 12 = 31-20+1 (taille de First Level tabe index dans l'adresse logique)
-#define FIRST_LVL_TT_SIZE	4096*SECOND_LVL_TT_SIZE // => 1 048 576 ko : Nombre d'entrées des tables de pages de niveau 1 multiplié par la taille d'une table de pages de niveau 2
-#define TOTAL_TT_SIZE		//4096*FIRST_LVL_TT_SIZE <== Ca fait beaucoup... Environ 4 To... 
+#define FIRST_LVL_TT_SIZE	FIRST_LVL_TT_COUN*4 // 16 384 o 
+#define TOTAL_TT_SIZE		FIRST_LVL_TT_SIZE + SECOND_LVL_TT_SIZE*FIRST_LVL_TT_COUN // 16 793 600 o
+
+uint32_t device_flags;
 
 
